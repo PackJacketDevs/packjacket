9ff014ca64d08fd77fe3bf6059b60c125c198ddb
Manodasan Wignarajah <manodasan@users.sourceforge.net>
2012-08-14 21:31:57 -0400

    Fixed PJ Helper

--- PJHelper/src/Main.java
+++ PJHelper/src/Main.java
@@ -1,4 +1,20 @@
-
+/*
+ * PackJacket - GUI frontend to IzPack to make Java-based installers
+ * Copyright (C) 2008 - 2012  Amandeep Grewal, Manodasan Wignarajah
+ *
+ * PackJacket is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PackJacket is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PackJacket.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -6,9 +22,21 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
 
+/**
+ * Sets up PackJacket resource files
+ * @author Amandeep Grewal
+ * @author Manodasan Wignarajah
+ */
 public class Main {
 
+    /*
+     * Determines whether the file a directory, or file and calls appropriate helper function
+     * @param src source file to copy from
+     * @param dest destination file to copy to
+     * @throws IOException 
+     */
     public static void copy(File src, File dest) throws IOException {
+        // Check for directory, or file
         if (src.isDirectory()) {
             copyDirectory(src, dest);
         } else {
@@ -16,7 +44,14 @@ public class Main {
         }
     }
 
+    /**
+     * Copies specified source directory to destination directory
+     * @param srcDir the source directory to copy
+     * @param destDir the destination directory to copy to
+     * @throws IOException
+     */
     public static void copyDirectory(File srcDir, File destDir) throws IOException {
+        destDir.mkdirs();
         for (File f : srcDir.listFiles()) {
             String filePath = f.getAbsolutePath();
             filePath = filePath.replace(srcDir.getAbsolutePath(), destDir.getAbsolutePath());
@@ -24,6 +59,12 @@ public class Main {
         }
     }
 
+    /**
+     * Copies specified file from source to destination
+     * @param srcFile the source file to copy
+     * @param destFile the destination to copy file to
+     * @throws IOException 
+     */
     public static void copyFile(File srcFile, File destFile) throws IOException {
         if (!destFile.exists()) {
             destFile.createNewFile();
@@ -48,22 +89,36 @@ public class Main {
         }
     }
 
+    /**
+     * Recursively deletes all file if directory, or deletes file
+     * @param toDelete file to delete
+     */
     public static void deleteFiles(File toDelete) {
+        // checks if file is directory to recursively delete
         if (toDelete.isDirectory()) {
             for (File f : toDelete.listFiles()) {
                 deleteFiles(f);
             }
         }
+        
+        // deletes file
         toDelete.delete();
     }
 
+    /**
+     * Main function - deletes existing resource files, and copies new ones
+     * @param paramArrayOfString contains directory to copy files from
+     * @throws IOException 
+     */
     public static void main(String[] paramArrayOfString) throws IOException {
         String str = System.getProperty("user.home") + "/.packjacket/";
 
+        // deletes existing files
         if (new File(str).exists()) {
             deleteFiles(new File(str));
         }
 
+        // copies new resource files
         new File(str + "launch4j").mkdirs();
         copy(new File(paramArrayOfString[0] + "standalone-compiler.jar"), new File(str + "standalone-compiler.jar"));
         copy(new File(paramArrayOfString[0] + "launch4j.xml"), new File(str + "launch4j.xml"));
@@ -77,6 +132,8 @@ public class Main {
         for (File localFile : new File(paramArrayOfString[0] + "l4j-common").listFiles()) {
             copy(localFile, new File(str + "launch4j/" + localFile.getName()));
         }
+        
+        // makes required resource files executable
         for (File localFile : new File(str + "launch4j/bin/").listFiles()) {
             localFile.setExecutable(true);
         }
